8d86b8e494311042ae7d5478abe8282ef9d13b96
Angelos Barmpoutis <angelbar@ufl.edu>
2023-09-15 12:22:19 -0400

    Update main.js

--- main.js
+++ main.js
@@ -16,35 +16,39 @@ var main=function(args){
 	var wind=args.app.getWindow();//gets the window object, which has many parameters, methods, and listeners
 	//for example:
 	wind.whenResized().then((wind)=>{
-		if(area) area.innerHTML="Window resized!";
+		//if(area) area.innerHTML="Window resized!";
 	})
+
+
+
 
 	//We create a GUI menu bar with one menu item:
 	var menulayout=new MenuLayout();
 	menulayout.getMenuBar().append(new MenuItem('Help')).whenClicked().then((item)=>{
-		area.innerHTML="I clicked Help!";
+		//area.innerHTML="I clicked Help!";
 	});
 
 	//You can append GUI elements to the window like this:
 	wind.getContent().append(menulayout);
 
-	//Or alternatively you can append custom divs made with vanilla JS:
-	var area=document.createElement('div');
-	opn.set(area.style,{
-		position:"absolute",
-		display:"block",
-		boxSizing:"border-box",
-		width:"100%",
-		height:"100%",
-		padding:"10px",
-		background:"linear-gradient(to bottom,blue 0%, white 100%)",
-		overflow:"hidden",
-		color:"white",
-		fontWeight:"700",
-		fontSize:"28px",
-		fontFamily:"Arial"
-	});
-	menulayout.getContainer().div.appendChild(area);
-	area.innerHTML="Hello World!";
+	
+
+	let toolbar_split=new SplitLayout({orientation:'vertical',sticky:'first',editable:false});
+	toolbar_split.setStickySize('auto','hidden');
+	menulayout.getContainer().append(toolbar_split);
+
+
+	let toolbar_layout=new HorizontalLayout();
+	toolbar_split.getFirstContainer().append(toolbar_layout);
+	toolbar_layout.append(new Button("Test"));
+
+
+	let left_split=new SplitLayout({orientation:'horizontal',sticky:'first'});
+	left_split.setStickySize('auto','hidden');
+	toolbar_split.getSecondContainer().append(left_split);
+
+	let left_panel=left_split.getFirstContainer().append(new VerticalLayout());
+
+	left_panel.append(new Button("test"));
 
 }

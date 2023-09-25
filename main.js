abd673f5bc4a7c14ba2281bd767a1e6094945540
Cindel Guerrero D'Agostini <cindel.guerrerod@ufl.edu>
2023-09-24 23:15:39 -0400

    changes to match weasis layout

--- main.js
+++ main.js
@@ -1,98 +1,69 @@
 preload(libs['GUI']).before(function(args){
-		//this is how you can optionally run scripts before loading your libraries.
-		args.app.showLoading();//Here we show the loading animation.
+		args.app.showLoading();
 	});
 
 preload("some_class.js");
+preload("app:9mkazv7tifp94mda@research.dwi.ufl.edu/op.n");//SlidingTabbedLayout class
 
 var main=function(args){
-	args.app.clearContents();//clears the loading animation
+	args.app.clearContents();
 
-	var wind=args.app.getWindow();//gets the window object, which has many parameters, methods, and listeners
+	var wind=args.app.getWindow();
 
-	//We create a GUI menu bar with three menu items:
 	var menulayout=new MenuLayout();
-	menulayout.getMenuBar().append(new MenuItem('File')).whenClicked().then((item)=>{
-		area.innerHTML="I clicked File!";
+	let fileButton=menulayout.getMenuBar().append(new MenuItem('File')).getSubMenu();
+
+	fileButton.append(new MenuItem("Open")).whenClicked().then((item)=>{
+		item.collapseMenu();
 	});
-	menulayout.getMenuBar().append(new MenuItem('View')).whenClicked().then((item)=>{
-		area.innerHTML="I clicked View!";
+	fileButton.append(new MenuItem("Import")).whenClicked().then((item)=>{
+		item.collapseMenu();
 	});
-	menulayout.getMenuBar().append(new MenuItem('Help')).whenClicked().then((item)=>{
-		area.innerHTML="I clicked Help!";
+	fileButton.append(new MenuItem("Export")).whenClicked().then((item)=>{
+		item.collapseMenu();
+	});
+	fileButton.append(new MenuItem("Print")).whenClicked().then((item)=>{
+		item.collapseMenu();
+	});
+	fileButton.append(new MenuItem("Preferences")).whenClicked().then((item)=>{
+		item.collapseMenu();
+	});
+	fileButton.append(new MenuItem("Exit")).whenClicked().then((item)=>{
+		item.collapseMenu();
 	});
 
-	//You can append GUI elements to the window like this:
+
+	let viewButton=menulayout.getMenuBar().append(new MenuItem('View')).getSubMenu();
+
+	viewButton.append(new MenuItem("Toolbars")).whenClicked().then((item)=>{
+		item.collapseMenu();
+	});
+	viewButton.append(new MenuItem("Tools")).whenClicked().then((item)=>{
+		item.collapseMenu();
+	});
+	viewButton.append(new MenuItem("Explorer")).whenClicked().then((item)=>{
+		item.collapseMenu();
+	});
+
+	let helpButton=menulayout.getMenuBar().append(new MenuItem('Help')).getSubMenu();
+
 	wind.getContent().append(menulayout);
 
-	var buttonArea=document.createElement('div');
-	opn.set(buttonArea.style,{
-		position:"static",
-		display:"inline-block",
-		boxSizing:"border-box",
-		background:"gray",
-		border:"5px",
-		borderColor:"black",
-		width:"20%",
-		height:"100%",
-		padding:"10px",
-		overflow:"hidden",
-		color:"white",
-		fontWeight:"700",
-		fontSize:"28px",
-		fontFamily:"Arial"
-	});
-	menulayout.getContainer().div.appendChild(buttonArea);
+	let toolbarSplit=new SplitLayout({orientation:'vertical',sticky:'first',editable:false});
+	toolbarSplit.setStickySize('auto','hidden');
+	menulayout.getContainer().append(toolbarSplit);
 
-	var explorerButton=document.createElement('button');
-	opn.set(explorerButton.style,{
-		position:"static",
-		display:"inline-block",
-		width:"20%",
-		height:"100%"
-	});
-	buttonArea.appendChild(explorerButton);
+	let toolbarLayout=new HorizontalLayout();
+	toolbarSplit.getFirstContainer().append(toolbarLayout);
 
-	var span=document.createElement('span');
-	opn.set(span.style,{
-		display:"block",
-		transform:"rotate(90deg)"
-	});
-	span.textContent="Explorer";
-	explorerButton.append(span);
+	let toolbarStyle={
+		applyStyle:function(button){
+			button.div.style.width="auto";
+		}
+	}
 
-	var smallWindow=document.createElement('window');
-	opn.set(smallWindow.style,{
-		position:"static",
-		display:"inline-block",
-		width:"55%",
-		height:"100%",
-		//padding:"10px",
-		background:"white",
-		color:"gray",
-		fontWeight:"700",
-		fontSize:"28px",
-		fontFamily:"Arial",
-		verticalAlign:"right"
-	});
-	buttonArea.appendChild(smallWindow);
+	let leftPanel=new SlidingTabbedLayout({side:'left'});
+	leftPanel.newTab('Image Explorer',new VerticalLayout()).makeHoverable();
 
-	//Or alternatively you can append custom divs made with vanilla JS:
-	var area=document.createElement('div');
-	opn.set(area.style,{
-		position:"static",
-		display:"inline-block",
-		boxSizing:"border-box",
-		width:"80%",
-		height:"100%",
-		padding:"10px",
-		background:"gray",
-		overflow:"hidden",
-		color:"white",
-		fontWeight:"700",
-		fontSize:"28px",
-		fontFamily:"Arial"
-	});
-	menulayout.getContainer().div.appendChild(area);
-	area.innerHTML="Hello World!";
+	toolbarSplit.getSecondContainer().append(leftPanel);
 }
